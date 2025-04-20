0ad4a359fc019a9985d0d038c0b170094fec8087
Priyam20Chatterjee <chatterjeepriyam96@gmail.com>
2025-04-20 23:21:00 +0530

    Calender Functionality Added and Updated.

--- src/components/calender/Header.jsx
+++ src/components/calender/Header.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
-
+import { useState } from 'react'
+import CommonCalendar from '../dashboard/CommonCalendar'
+import WeekCalendar from '../calender/week/WeekCalender'
 const Header = () => {
+    const[isToggleMonthWeekView, setIsToggleMonthWeekView] = useState(true)
+  
   return (
+    <>
     <section className='flex justify-between'>
         <div className='mt-5 ml-5 flex flex-col gap-2'>
             <p className='text-black text-3xl font-semibold'>Calender</p>
             <h6 className='text-gray-500'>Monthly and Weekly View of your tasks.</h6>
         </div>
-        <div className='mt-4 mr-3 pl-3 pr-3 rounded-full flex bg-gray-200 gap-3'>
-            <button className='bg-white p-3 rounded-full mt-3 mb-3 tracking-wide text-blue-700 font-medium'>Month</button>
-            <button className='bg-white p-3 rounded-full mt-3 mb-3 tracking-wide text-blue-700 font-medium'>Week</button>
+        <div className='mt-4 mr-3 pl-3 pr-3 rounded-full flex bg-gray-200 gap-3'>  
+         
+           {/* <button className='bg-white p-3 rounded-full mt-3 mb-3 tracking-wide text-blue-700 font-medium'>Month</button> */}
+
+            <button
+               onClick={() => setIsToggleMonthWeekView(true)}
+               className={`p-3 rounded-full mt-3 mb-3 tracking-wide font-medium 
+               ${isToggleMonthWeekView ? 'bg-blue-700 text-white' : 'bg-white text-blue-700'}`}
+             >
+             Month
+            </button>
+            {/* <button className='bg-white p-3 rounded-full mt-3 mb-3 tracking-wide text-blue-700 font-medium'>Week</button> */}
+
+            <button
+               onClick={() => setIsToggleMonthWeekView(false)}
+               className={`p-3 rounded-full mt-3 mb-3 tracking-wide font-medium 
+               ${!isToggleMonthWeekView ? 'bg-blue-700 text-white' : 'bg-white text-blue-700'}`}
+            >
+            Week
+            </button>
+
         </div>
     </section> 
+    <div className='p-5 '>
+       {isToggleMonthWeekView ? <CommonCalendar /> : <WeekCalendar/>}
+    </div>
+    </>
   )
 }
 
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import LeftNav from '../components/shared/LeftNav'
 import TopNav from '../components/shared/TopNav'
 import Header from '../components/calender/Header'
-// import WeekCalendar from '../components/calender/week/WeekCalender'
-import CommonCalendar from '../components/dashboard/CommonCalendar'
 
 const Calendar = () => {
+
   return (
     <section className='flex h-screen overflow-hidden'>
     <div className='flex-[1]'>
@@ -16,16 +15,6 @@ const Calendar = () => {
 
       <div className='overflow-y-scroll h-[calc(100vh-64px)]'>
         <Header/>
-        {/* <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-          <WeekCalendar />
-        </div> */}
-
-        <div className='p-5 bg-gray-100'>
-          <CommonCalendar />
-        </div>
-
-        
-
       </div>
     </div>
   </section>
